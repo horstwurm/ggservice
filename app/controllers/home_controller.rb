2b670fbdb8e253b751d2d7697de3656383587e74
horstwurm <horst.wurm@bluewin.ch>
2016-10-23 16:28:15 +0000

    second

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
-    if params[:name]
+    if params[:name] and params[:vorname] and params[:pw] and params[:name].length > 0 and params[:vorname].length >0 and params[:pw].length>0
       redirect_to taxes_path :name => params[:name], :vorname => params[:vorname], :pw => params[:pw]
+    else
+      @message = "bitte alle drei Felder ausfüllen!"
+      #redirect_to home_index_path
     end 
   end
 end
